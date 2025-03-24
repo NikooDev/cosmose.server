c394c2bf00e6ecb0a46170243c8419f2ead2bd6f
nicolas <nicolas.tual@outlook.fr>
2025-03-24 19:31:47 +0100

    First commit

--- server.ts
+++ server.ts
@@ -3,9 +3,14 @@ import routes from '@Server/routes';
 import cors from 'cors';
 
 const app = express();
-const port = 8121;
+const port = 3000;
 
 app.use(express.json());
+
+app.get('/', (_req, res: express.Response) => {
+	res.setHeader("Content-Type", "application/json");
+	res.json({ name: 'Hello world' });
+});
 
 app.use(cors({
 	origin: ['http://localhost:4000', 'https://cosmoseapp.vercel.app'],
@@ -18,3 +23,5 @@ app.use(routes);
 app.listen(port, () => {
 	console.log(`Server running on http://localhost:${port}`);
 });
+
+export default app;
