52f1ea233592fc7d762c1b6020aa0bccb6f5abef
nicolas <nicolas.tual@outlook.fr>
2025-03-24 18:58:13 +0100

    First commit

--- dist/src/services/firebase.service.js
+++ dist/src/services/firebase.service.js
@@ -35,11 +35,22 @@ var __importStar = (this && this.__importStar) || (function () {
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
 const dotenv = __importStar(require("dotenv"));
-const cosmoseapp_json_1 = __importDefault(require("../private/cosmoseapp.json"));
 dotenv.config();
+const firebaseConfig = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: (_a = process.env.FIREBASE_PRIVATE_KEY) === null || _a === void 0 ? void 0 : _a.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 class FirebaseService {
     static get Auth() {
         return FirebaseService.auth;
@@ -51,7 +62,7 @@ class FirebaseService {
 (() => {
     if (!firebase_admin_1.default.apps.length) {
         firebase_admin_1.default.initializeApp({
-            credential: firebase_admin_1.default.credential.cert(cosmoseapp_json_1.default),
+            credential: firebase_admin_1.default.credential.cert(firebaseConfig),
         });
     }
     FirebaseService.auth = firebase_admin_1.default.auth();
